3c6a22ff02ee9d38f84015c812008884450625a0
testacct-knx <gcpacguser@gmail.com>
2025-06-16 20:31:17 -0400

    Update streamlit_app.py
    Updating from SIS to SniS by changing the connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-session = get_active_session()
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
