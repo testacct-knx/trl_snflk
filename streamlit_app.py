c9cec36a92c678ef28977777f57c5db0fdff2d15
testacct-knx <gcpacguser@gmail.com>
2025-06-17 12:11:40 -0400

    add json response to streamlit page

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -45,3 +44,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
     
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
+
+# Display smoothiefroot nutrition info
+smoothiefroot_respoonse = requests.get("https://www.smoothiefroot.com/api/fruit/orange")
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
