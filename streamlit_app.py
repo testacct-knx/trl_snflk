2a2c69ef4c0ae0489a3f19085bc0b29b3bf6bfec
testacct-knx <gcpacguser@gmail.com>
2025-06-16 20:04:49 -0400

    Update streamlit_app.py
    Migrating from SIS to SNIS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
-session = get_active_session()
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Write directly to the app
 st.title(f":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
