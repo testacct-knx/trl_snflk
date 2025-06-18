324740ca98130f3b13ef3123c19b9e288a3ef242
testacct-knx <gcpacguser@gmail.com>
2025-06-18 14:23:55 -0400

    adding search term

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:", max_chars=100)
 st.write("The name on your smoothie will be: ", name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('search_on'))
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
