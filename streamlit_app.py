e5cd0b9081a8477d22e3e41b3a9455ca7b336358
testacct-knx <gcpacguser@gmail.com>
2025-06-18 14:32:22 -0400

    building pandas df and search printout

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,24 +19,26 @@ st.write("The name on your smoothie will be: ", name_on_order)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('search_on'))
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
+# st.dataframe(pd_df)
 
+
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:'
+    , my_dataframe
+    , max_selections=5
+)
+
+
+if ingredients_list:
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.subheader(f'The search value for {fruit_chosen} is {search_on}')
 
 st.stop()
-# ingredients_list = st.multiselect(
-#     'Choose up to 5 ingredients:'
-#     , my_dataframe
-#     , max_selections=5
-# )
-
-
-# if ingredients_list:
-#     ingredients_string = ''
-#
-#     for fruit_chosen in ingredients_list:
-#         ingredients_string += fruit_chosen + ' '
-#
-#         st.subheader(f'{fruit_chosen} Nutrition Information')
 #         fruit_api_loc = f"https://www.smoothiefroot.com/api/fruit/{str.lower(fruit_chosen)}"
 #
 #         smoothiefroot_respoonse = requests.get("")
